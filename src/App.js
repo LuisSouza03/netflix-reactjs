4d71ea178f49ad6365db8e4a1042157035e9a986
luiigii <luis93667@gmail.com>
2021-09-08 22:57:20 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React, { useEffect, useState } from 'react';
 import './App.css'
 import Tmdb from './Tmdb'; // Importando as Listas de Filmes que vem de Tmdb
 import MovieRow from './components/MovieRow' // Importando o MovieRow que seria a Lista de Filmes
-
+import FeaturedMovie from './components/FeaturedMovie'
 
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       // Pegando Lista dos Filmes
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Depois que tem a lista, pegamos o filme em destaque
+      let originals = list.filter(i => i.slug === 'originals'); // Pegando os Originais da Netflix em Tmdb
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+      
     }
 
     loadAll();
@@ -21,6 +30,12 @@ export default () => {
 
   return(
     <div className="page">
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} /> // Key Item necessário, se não ele da erro // E pegamos os itens do JSON do Tmdb
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -57,5 +57,24 @@ export default {
                 items: await basicFecth(`/discover/movie?with_genres=9648&language=pt-BR&api_key=${API_KEY}`)
             },
         ]
+    },
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+
+        if(movieId) {
+            switch(type) {
+                case 'movie':
+                    info = await basicFecth(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`)
+                break;
+                case 'tv':
+                    info = await basicFecth(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`)
+                break;
+                default:
+                    info = null;
+                break;
+            }
+        }
+
+        return info;
     }
 }
